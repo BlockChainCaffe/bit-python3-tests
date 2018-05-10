d959bc676494c633f79e2f24d125d53bde4adbbe
emcy <marco.crotta@gmail.com>
2018-05-10 13:40:19 +0200

    some more tests

--- keytest.py
+++ keytest.py
@@ -7,13 +7,23 @@ https://ofek.github.io/bit/index.html
 
 import sys
 import bit
+import hashlib
 from bit import utils
 
 
 def main():
-    key = bit.Key.from_hex('a0dc65ffca799873cbea0ac274015b9526505daaaed385155425f7337704883e')
+	# echo -n "Satoshi Nakamoto" | sha256sum
+    # key = bit.Key.from_hex('a0dc65ffca799873cbea0ac274015b9526505daaaed385155425f7337704883e')
     # key = bit.Key.from_bytes(b'Satoshi Nakamoto')
-    print("Private key: ", key)
+
+    seed = ''.join(sys.argv[1:])
+    priv = hashlib.sha256(seed.encode('utf-8')).hexdigest()
+    key = bit.Key.from_hex(priv)
+	 
+    #priv="a0dc65ffca799873cbea0ac274015b9526505daaaed385155425f7337704883e"
+    #key = bit.Key.from_hex(priv)
+
+    print("Private key: ", priv)
     print("Public key:  ", utils.bytes_to_hex(key.public_key, True))
     print("WIF:         ", key.to_wif())
     print("Address:     ", key.address)
